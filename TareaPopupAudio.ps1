beedb88367542e88df3f51903451a62bfe233b7c
JoseHerrera00 <99744643+JoseHerrera00@users.noreply.github.com>
2025-09-29 09:17:08 -0500

    Add files via upload

---
+++ TareaPopupAudio.ps1
@@ -0,0 +1,20 @@
+# Descargar el script desde GitHub y guardarlo como popupaudio.ps1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/JoseHerrera00/popupscrip/main/popupaudio.ps1" -OutFile "C:\Temp\popupaudio.ps1" -UseBasicParsing
+
+# Descargar el video.mp4 o el audio que se defina (usar BITS transfer para archivos grandes)
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/JoseHerrera00/popupscrip/main/Popupvideo.mp4" -Destination "C:\Temp\Popupvideo.mp4"
+
+# Definir la hora a la que quieres que se ejecute la tarea (ejemplo: 16:49)
+$horaEjecucion = Get-Date -Hour 11 -Minute 54 -Second 0
+
+# Crear la acción para ejecutar el script con PowerShell
+$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -WindowStyle Normal -File "C:\Temp\popupaudio.ps1"'
+
+# Crear el trigger para que se ejecute solo una vez en la fecha y hora especificadas
+$Trigger = New-ScheduledTaskTrigger -Once -At $horaEjecucion
+
+# Obtener el usuario actual para ejecutar la tarea con ese usuario
+$usuario = (Get-WmiObject -Class Win32_ComputerSystem).UserName
+
+# Registrar o reemplazar la tarea programada con privilegios elevados
+Register-ScheduledTask -TaskName "InteractiveTask" -Action $Action -Trigger $Trigger -RunLevel Highest -User $usuario -Force
